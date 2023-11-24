f49be57d4049984fe47533a75ac798f3890a865e
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2023-11-24 14:34:20 -0800

    Support migrating from an earlier version

--- OneItemForFree_0.0.5/control.lua
+++ OneItemForFree_0.0.5/control.lua
@@ -10,8 +10,8 @@ function show_item_picker_gui(player)
             type = "choose-elem-button",
             name = "super_chest_selection",
             elem_type = "item",
-            elem_filters = {{filter="name", name="super-chest", invert=true}},
-            tags = {super_chest_selection = true}
+            elem_filters = { { filter = "name", name = "super-chest", invert = true } },
+            tags = { super_chest_selection = true }
         }
     end
 end
@@ -33,7 +33,6 @@ script.on_event(defines.events.on_gui_elem_changed, function(event)
     end
 end)
 
-
 local function give_super_chest(player)
     player.get_main_inventory().insert { name = "super-chest", count = 1 }
     show_item_picker_gui(player)
@@ -50,22 +49,43 @@ script.on_init(function()
     end
 end)
 
+function set_filter(chest, chosen_item)
+    if not chosen_item then
+        return
+    end
+    local stacks = chest.prototype.get_inventory_size(defines.inventory.chest)
+    local stack_size = game.item_prototypes[chosen_item].stack_size
+    chest.infinity_container_filters = {
+        {
+            name = chosen_item,
+            count = stacks * stack_size,
+            mode = "exactly",
+            index = 1
+        }
+    }
+end
+
 -- Check when the player places a chest, and set the infinite item if relevant
 script.on_event(defines.events.on_built_entity, function(event)
     local entity = event.created_entity
     if entity and entity.valid and entity.name == "super-chest" then
         local chosen_item = global.player_choices[event.player_index]
-        if chosen_item then
-            local stacks = entity.prototype.get_inventory_size(defines.inventory.chest)
-            local stack_size = game.item_prototypes[chosen_item].stack_size
-            entity.infinity_container_filters = {
-                {
-                    name = chosen_item,
-                    count = stacks * stack_size,
-                    mode = "exactly",
-                    index = 1
-                }
-            }
+        set_filter(entity, chosen_item)
+    end
+end)
+
+script.on_configuration_changed(function(data)
+    if not (data.mod_changes and data.mod_changes["OneItemForFree"] and data.mod_changes["OneItemForFree"].old_version) then
+        return
+    end
+    if global.super_chest_item_name then
+        for _, data in global.super_chest_item_name do
+            local chest = data.surface.find_entity("super-chest", data.position)
+            if chest then
+                set_filter(chest, data.chosen_item)
+            end
         end
     end
+    global.super_chest_item_name = nil
+    global.players_given_super_chest = nil
 end)
